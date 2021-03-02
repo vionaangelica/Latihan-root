56196c2ca7d8062411d0762dabedfff60e0a29b4
vionaangelica <c14180157@john.petra.ac.id>
2021-03-02 16:21:44 +0700

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,17 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
+import { LoginComponent } from './login/login.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const ROUTES : Routes = [
+  {path : 'login', component : LoginComponent}
+]
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule, RouterModule.forRoot(ROUTES)],
+  
+  declarations: [ AppComponent, HelloComponent, LoginComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
