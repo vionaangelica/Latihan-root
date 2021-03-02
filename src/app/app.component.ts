9557206d71433211880010832f8a750ae75fd54d
vionaangelica <c14180157@john.petra.ac.id>
2021-03-02 20:00:08 +0700

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,18 +8,17 @@ import { GlobalvarService } from "./globalvar.service";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  
   name = "Angular " + VERSION.major;
 
-  constructor(private router: Router, private globalvar : GlobalvarService) {
+  constructor(private router: Router, private globalvar: GlobalvarService) {
     this.namaGlobal = this.globalvar.getNama();
   }
-  namaGlobal
-  textNama = ""  
+  namaGlobal;
+  textNama = "";
 
   OpenHalLogin() {
     this.globalvar.setNama(this.textNama);
     this.namaGlobal = this.globalvar.getNama();
-    this.router.navigate(["/home/"+this.textNama]);
+    this.router.navigate(["/home/" + this.textNama]);
   }
 }
